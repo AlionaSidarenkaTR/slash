73e73c9b9e5115d3ec6a8455e54434dff7629e3d
slash-slash <bigben.by@gmail.com>
2016-10-16 17:13:55 +0300

    Update auth.js

--- wunderlist2/server/config/auth.js
+++ wunderlist2/server/config/auth.js
@@ -1,13 +1,13 @@
 module.exports = {
 	'facebookAuth': {
-		'clientID': '448891418649181',
-		'clientSecret': '815c70ddfd11754a29c6ac2a3714f74a',
+		'clientID': '',
+		'clientSecret': '',
 		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
 	},
 
 	'googleAuth': {
-		'clientID': '1059209882951-m21tagkpqqjk8lf1850hus00qnjv5pdu.apps.googleusercontent.com',
-		'clientSecret': 'avRN7XrdZ38aAvi_UZO18ZDv',
+		'clientID': '',
+		'clientSecret': '',
 		'callbackURL': 'http://localhost:8080/auth/google/callback'
 	}
 }
